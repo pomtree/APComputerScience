439f26a34a5c0d29791a77d4dc857719def611dd
pomtree <im-the@hot-shot.com>
2018-03-01 21:17:40 -0800

    Finished most of tictactoe

--- src/tom/pree/TicTacToe.java
+++ src/tom/pree/TicTacToe.java
@@ -1,17 +1,27 @@
 package tom.pree;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TicTacToe {
     public static int[][] b;
+    static boolean firstRun;
 
     // 0 = blank, 1 = X; 2 = O;
     public static void main(String args[]) {
+        firstRun = true;
+        driver();
+        System.out.println("Goodbye");
+    }
+
+    public static void driver() {
+        if (!firstRun) System.out.println("\n\n\n\n\n\n");
+        System.out.println("Welcome to TicTacToe!");
         Scanner s = new Scanner(System.in);
         b = new int[3][3];
         boolean x = true; //x (true) = x's turn
-        print();
-        while (hasWon() != 0) {
+        //print();
+        while (hasWon() == 0) {
             if (x) {
                 go(x, s);
             } else {
@@ -19,13 +29,77 @@ public class TicTacToe {
             }
             x = !x;
         }
+        if (hasWon() == 1) System.out.println("X's has won!!");
+        else System.out.println("O's have won!");
+        System.out.print("Would you like to play again? (y or n): ");
+        char c = (s.nextLine().toUpperCase() + " ").charAt(0);
+        if (c == 'Y') {
+            driver();
+        }
     }
 
     public static void go(boolean x, Scanner s) {
+        if (!firstRun) System.out.println("\n\n\n");
+        print();
         if (x) {
             System.out.println("It's X's turn.");
-        } else System.out.println("It's O's turn.");
+        } else {
+            System.out.println("It's O's turn.");
+        }
+        int[] coord = getPos(s);
+        if (x) {
+            b[coord[0]][coord[1]] = 1;
+        } else {
+            b[coord[0]][coord[1]] = 2;
+        }
+    }
 
+    public static int[] getPos(Scanner s) {
+        int row = -1;
+        while (row == -1) {
+            System.out.print("Enter your desired row: ");
+            try {
+                row = s.nextInt();
+                if (row > 3 || row < 1) {
+                    throw new InputMismatchException();
+                }
+                //System.out.println(row);
+            } catch (InputMismatchException e) {
+                System.out.println("You entered an invalid row, please try again. (1-3)");
+                row = -1;
+            }
+        }
+        int col = -1;
+        while (col == -1) {
+            System.out.print("Enter your desired column: ");
+            if (!firstRun) s.nextLine();
+            else firstRun = false;
+            try {
+                col = s.nextInt();
+                if (col > 3 || col < 1) {
+                    throw new InputMismatchException();
+                }
+                //System.out.println(col);
+            } catch (InputMismatchException e) {
+                System.out.println("You entered an invalid column, please try again. (1-3)");
+                col = -1;
+            }
+        }
+        row--;
+        col--;
+        if (b[row][col] != 0) {
+            System.out.println("Sorry, that spot is taken. Try again.");
+            return getPos(s);
+        }
+
+        System.out.print("You entered " + (row + 1) + "," + (col + 1) + ". Is this OK?( y or n): ");
+        s.nextLine();
+        char c = (s.nextLine().toUpperCase() + " ").charAt(0);
+        if (c != 'Y') {
+            return getPos(s);
+        }
+
+        return new int[]{row, col};
     }
 
 
